87cd25022e8f61a990332b7e6a3d9836f2649f8c
krisszcode <nagy.ace.krisztian@gmail.com>
2020-11-02 19:21:42 +0100

    make basic layout and do it's logic in the Router.php

--- core/Router.php
+++ core/Router.php
@@ -30,11 +30,30 @@ class Router
         $path = $this->request->getPath();
         $method = $this->request->getMethod();
         $callback = $this->routes[$method][$path] ?? false;
-        var_dump($callback);
         if($callback === false){
-            echo "Not found";
-            exit();
+            return "Not found";
         }
-        echo call_user_func($callback);
+        if(is_string($callback)){
+            return $this->renderView($callback);
+        }
+        return call_user_func($callback);
+    }
+
+    public function renderView($view){
+        $layoutContent = $this->layoutContent();
+        $viewContent = $this->renderOnlyView($view);
+        return str_replace('{{content}}', $viewContent, $layoutContent);
+    }
+
+    protected function layoutContent(){
+        ob_start();
+        include_once Application::$ROOT_DIR."/views/layouts/main.php";
+        return ob_get_clean();
+    }
+
+    protected function renderOnlyView($view){
+        ob_start();
+        include_once Application::$ROOT_DIR."/views/$view.php";
+        return ob_get_clean();
     }
 }
